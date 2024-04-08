8922924d2746bf9ed6619dcba3af7a0654ab8fc5
mohamed-aziz <medazizzammal@gmail.com>
2024-04-08 05:08:41 +0100

    Add React frontend for random quote generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [type, setType] = useState("");
+  const [quote, setQuote] = useState(""); // حالة لتخزين الاقتباس
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const response = await Axios.post("http://127.0.0.1:5000/quote", {
+      type
+    });
+    setQuote(response.data.quote); // تحديث الاقتباس بالبيانات المسترجعة
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <h1>Random Quote</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="quote_type">Choose a quote type:</label>
+        <select
+          name="quote_type"
+          id="quote_type"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option value="science">Science</option>
+          <option value="life">Life</option>
+          <option value="love">Love</option>
+        </select>
+        <button type="submit">Get Quote</button>
+      </form>
+      {/* عرض الاقتباس إذا تم استرجاعه */}
+      {quote && (
+        <div>
+          <h2>Quote:</h2>
+          <p>{quote}</p>
+        </div>
+      )}
+    </>
   );
 }
 
